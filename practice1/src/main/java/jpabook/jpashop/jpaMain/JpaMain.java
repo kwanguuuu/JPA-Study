72d7987d77817b2a31b1da5b84f65b8bc8351fdf
kwanguuuu <kwangyu326@gmail.com>
2019-10-14 23:26:02 +0900

    init practice

---
+++ practice1/src/main/java/jpabook/jpashop/jpaMain/JpaMain.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.jpaMain;
+
+import jpabook.jpashop.domain.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em =emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+        try {
+            tx.commit();    //이 때 커밋으로 넘어감
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+
+        }
+
+        emf.close();    //스키 자동생성이 create-drop일 떄, 드롭이 이거 수행 후 이뤄짐.
+
+
+    }
+}
